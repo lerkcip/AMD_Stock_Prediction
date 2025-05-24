49cdaf364f8549dc6072130c4a658f5abf490bb1
lerkcip <jpickrelsmith@my365.bellevue.edu>
2025-05-24 18:12:11 -0500

    Remove unnecessary REDDIT_USER_AGENT references

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -22,8 +22,7 @@ def fetch_reddit_data(subreddits=['AMD_Stock', 'StockMarket', 'stocks', 'investi
     # Initialize Reddit API client
     reddit = praw.Reddit(
         client_id=os.environ.get('REDDIT_CLIENT_ID'),
-        client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
-        user_agent=os.environ.get('REDDIT_USER_AGENT')
+        client_secret=os.environ.get('REDDIT_CLIENT_SECRET')
     )
     
     # Get today's date
